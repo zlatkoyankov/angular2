95b8200e012ec0234875026e3c38e53e3be55412
Zlatko Yankov <zlatkoyankov@gmail.com>
2016-06-12 13:06:14 +0300

    delete files from final project to make it as it should start

--- app/home/welcome.component.ts
+++ app/home/welcome.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from 'angular2/core';
 
 @Component({
     templateUrl: 'app/home/welcome.component.html'
 })
 export class WelcomeComponent {
-    public pageTitle: string = 'Welcome';
+    public pageTitle: string = "Welcome";
 }
