adb64bec41e7ffd02f43bc2f49bf9470458ccc65
Zlatko Yankov <zlatkoyankov@gmail.com>
2016-06-14 00:03:30 +0300

    angular2 tutorial till the 5. service

---
+++ app/hero.js
@@ -0,0 +1,9 @@
+"use strict";
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Hero;
+//# sourceMappingURL=hero.js.map
